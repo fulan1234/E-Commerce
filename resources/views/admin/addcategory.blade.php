01e3b1196d7272e4191a14dba914b35cc821551b
fulan1234 <krazyflash10@gmail.com>
2023-06-23 08:39:48 +0700

    tambahin crud di add category

--- resources/views/admin/addcategory.blade.php
+++ resources/views/admin/addcategory.blade.php
@@ -12,7 +12,17 @@ Add Category - Single Ecom
             <small class="text-muted float-end">Input Information</small>
           </div>
           <div class="card-body">
-            <form action="" method="POST">
+            <form action="{{route('storecategory')}}" method="POST">
+              @csrf
+              @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="basic-default-name">Category Name</label>
                 <div class="col-sm-10">
